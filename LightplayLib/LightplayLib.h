5ada12e576dcacf69b6b028312dc8797f11c96da
ciara <csmobile99@gmail.com>
2024-03-12 18:00:48 -0700

    first commit

---
+++ LightplayLib/LightplayLib.h
@@ -0,0 +1,18 @@
+//
+//  LightplayLib.h
+//  LightplayLib
+//
+//  Created by ciara staggs on 3/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LightplayLib.
+FOUNDATION_EXPORT double LightplayLibVersionNumber;
+
+//! Project version string for LightplayLib.
+FOUNDATION_EXPORT const unsigned char LightplayLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightplayLib/PublicHeader.h>
+
+
---
+++ LightplayLibExamples/LightplayLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightplayLibExamplesApp.swift
+//  LightplayLibExamples
+//
+//  Created by ciara staggs on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LightplayLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
